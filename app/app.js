5ebf754c21fbac9e9776a5543bd7354423a50f5f
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-06-21 11:48:14 +0200

    Remove unneeded dependencies

--- app/app.js
+++ app/app.js
@@ -1,7 +1,6 @@
 import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
-import browserUpdate from 'browser-update';
 import config from './config/environment';
 import './config/custom-inflector-rules';
 import { setupSentry } from 'frontend-lpdc/utils/sentry';
@@ -20,13 +19,6 @@ export default class App extends Application {
 // More information: https://github.com/emberjs/ember.js/issues/19289
 Object.defineProperty(Array.prototype, '_super', {
   enumerable: false,
-});
-
-browserUpdate({
-  vs: { i: 11, f: -3, o: -3, s: -3, c: -3 },
-  style: 'corner',
-  l: 'nl',
-  shift_page_down: false,
 });
 
 loadInitializers(App, config.modulePrefix);

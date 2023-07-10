e585a365dd4d349227d6d5b2bc415232b0c232c4
gauquiebart <bart.gauquie@gmail.com>
2023-07-10 16:31:23 +0200

    LPDC-540: include relationships in public-service index.js query

--- app/routes/public-services/index.js
+++ app/routes/public-services/index.js
@@ -1,6 +1,6 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
-import { restartableTask } from 'ember-concurrency';
+import {inject as service} from '@ember/service';
+import {restartableTask} from 'ember-concurrency';
 
 export default class PublicServicesIndexRoute extends Route {
   @service store;
@@ -53,32 +53,8 @@ export default class PublicServicesIndexRoute extends Route {
       query['filter[:has:review-status]'] = true;
     }
 
-    let publicServices = yield this.store.query('public-service', query);
+    query['include'] = 'target-audiences,type,executing-authority-levels,status';
 
-    let promises = [];
-    publicServices.forEach((service) => {
-      promises.push(
-        service.hasMany('targetAudiences').reload(),
-        service.hasMany('executingAuthorityLevels').reload(),
-        service.belongsTo('type').reload()
-      );
-    });
-    yield Promise.all(promises);
-
-    // TODO: We've split this up into separate promise arrays to work around a bug in Ember Data 4.12.0
-    // Move this back into the forEach loop once the fix is released (supposedly in 4.12.1): https://github.com/emberjs/data/pull/8597
-    yield Promise.all(
-      publicServices.map((service) => service.belongsTo('status').reload())
-    );
-
-    // TODO: We've split this up into separate promise arrays to work around a bug in Ember Data 4.12.0
-    // Move this back into the forEach loop once the fix is released (supposedly in 4.12.1): https://github.com/emberjs/data/pull/8597
-    yield Promise.all(
-      publicServices.map((service) =>
-        service.belongsTo('reviewStatus').reload()
-      )
-    );
-
-    return publicServices;
+    return yield this.store.query('public-service', query);
   }
 }

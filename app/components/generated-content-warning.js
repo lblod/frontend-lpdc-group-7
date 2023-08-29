a265a9dd5ad2d02a204959e174ba069aa9ee6969
pdhondt <44711935+pdhondt@users.noreply.github.com>
2023-08-29 11:01:40 +0200

    LPDC-634: removed asterisks and spaces from emailBody

--- app/components/generated-content-warning.js
+++ app/components/generated-content-warning.js
@@ -18,29 +18,25 @@ export default class GeneratedContentWarning extends Component {
   }
 
   get emailBody() {
-    return `
-    * Melding probleem automatische omzetting van ${this.getConversionText()} in concept ${
-      this.args.conceptName
-    } (${this.args.conceptProductId}) %0D%0A
-    %0D%0A
-    * Probleem in veld(en) (kruis aan): %0D%0A
-      [ ] Beschrijving %0D%0A
-      [ ] Aanvullende beschrijving %0D%0A
-      [ ] Uitzonderingen %0D%0A
-      [ ] Voorwaarde %0D%0A
-      [ ] Bewijsstuk %0D%0A
-      [ ] Procedure %0D%0A
-      [ ] Beschrijving website procedure %0D%0A
-      [ ] Beschrijving website %0D%0A
-      [ ] Kost %0D%0A
-      [ ] Financieel voordeel %0D%0A
-      [ ] Regelgeving %0D%0A
-      [ ] Alle bovenstaande velden %0D%0A
-      [ ] Ander veld: %0D%0A
-    %0D%0A
-    * Beschrijving van het probleem (vul aan): %0D%0A
-    %0D%0A
-    `;
+    return `Melding probleem automatische omzetting van ${this.getConversionText()} in concept ${this.args.conceptName} (${this.args.conceptProductId})%0D%0A
+%0D%0A
+Probleem in veld(en) (kruis aan):%0D%0A
+[ ] Beschrijving%0D%0A
+[ ] Aanvullende beschrijving%0D%0A
+[ ] Uitzonderingen%0D%0A
+[ ] Voorwaarde%0D%0A
+[ ] Bewijsstuk%0D%0A
+[ ] Procedure%0D%0A
+[ ] Beschrijving website procedure%0D%0A
+[ ] Beschrijving website%0D%0A
+[ ] Kost%0D%0A
+[ ] Financieel voordeel%0D%0A
+[ ] Regelgeving%0D%0A
+[ ] Alle bovenstaande velden%0D%0A
+[ ] Ander veld:%0D%0A
+%0D%0A
+Beschrijving van het probleem (vul aan):%0D%0A
+%0D%0A`;
   }
 
   getConversionText() {

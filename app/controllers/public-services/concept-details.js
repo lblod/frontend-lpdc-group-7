da91e83903f4d9ef1e5aca83b99b0dd8520d181f
thuysbr <brent.thuys@kunlabora.be>
2023-11-27 12:40:44 +0100

    LPDC-793: Fix linting error

--- app/controllers/public-services/concept-details.js
+++ app/controllers/public-services/concept-details.js
@@ -1,6 +1,6 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { dropTask } from "ember-concurrency";
+import { dropTask } from 'ember-concurrency';
 import { inject as service } from '@ember/service';
 
 const ARCHIVED_STATUS =
